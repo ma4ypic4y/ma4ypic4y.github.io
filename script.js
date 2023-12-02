354d43b0f59bcd2d3fb8dfecb7c27151d2de3cc6
maxim <maksppap123@mail.ru>
2023-12-02 21:38:48 +0300

    true css less

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(document).on('touchstart click', 'nav ul li', function(){
-    var currTarget = $(this).attr('data-loc');
-    $('html, body').stop().animate({
-      scrollTop: $($('#' + currTarget)).offset().top
-    }, 500);
-  });
-});
